dbcfe797775629fd42c245d2e79a073c144d5b19
MoonLy03 <93345815+Finn331@users.noreply.github.com>
2024-04-16 11:11:00 +0700

    Initial Commit

---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float sprintSpeed; // Speed when sprinting
+    public float speed; // Current speed
+    public float jumpingPower;
+    private float defaultSpeed; // Default speed
+    private float horizontal;
+    private bool isFacingRight = true;
+
+    private bool isSprinting; // Added variable to track sprinting state
+
+    private int jumpsRemaining; // Number of jumps remaining
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Animator anim;
+
+    private PlayerStatus playerStatus;
+
+    private void Start()
+    {
+        defaultSpeed = speed;
+        jumpsRemaining = 2; // Initialize the number of jumps remaining to 2
+        playerStatus = GetComponent<PlayerStatus>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift)) // Check if "Shift" key is pressed
+        {
+            isSprinting = true;
+            speed = sprintSpeed; // Set the speed to sprint speed
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift)) // Check if "Shift" key is released
+        {
+            isSprinting = false;
+            speed = defaultSpeed; // Set the speed back to default
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Move the player horizontally
+        Move();
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        anim.SetTrigger("Walk");
+        
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            jumpsRemaining = 2; // Reset jumps remaining when grounded
+            anim.SetTrigger("Jump");
+        }
+        else if (jumpsRemaining > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            jumpsRemaining--; // Decrease jumps remaining when performing a double jump
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    void Die() // Not Implemented Yet
+    {
+        if (playerStatus.currHealth == 0)
+        {
+            // Animasi Death & Checkpoint Logic
+        }
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
---
+++ Assets/Script/Player/PlayerStatus.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public int maxHealth;
+    public int currHealth;
+
+    public int damage;
+
+    public int score;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI healthText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Health();
+        Score();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            score += 1;
+        }
+    }
+
+    public void Health()
+    {
+        healthText.text ="Health: " + currHealth.ToString();
+    }
+
+    public void Score()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
